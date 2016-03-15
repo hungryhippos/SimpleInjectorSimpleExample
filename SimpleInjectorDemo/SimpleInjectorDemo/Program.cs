2d006472824baea0d04c9f09be4ae981ba1b1572
hungryhippos <phillipknight@outlook.com>
2016-03-14 19:56:49 -0700

    added program.cs

---
+++ SimpleInjectorDemo/SimpleInjectorDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInjectorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new SimpleInjector.Container();
+            container.Register<IOrder, PurchaseOrder>();
+
+            var shoppingCart = container.GetInstance<ShoppingCart>();
+            shoppingCart.CheckOut();
+
+            Console.ReadLine();
+        }
+    }
+}
